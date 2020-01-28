c2383c90748e6ea0b1329c91701532245f1c6e7f
wuzengshun <823767678@qq.com>
2020-01-28 16:50:16 +0800

    代码中控制控件

--- helloworld/src/main/java/com/example/helloworld/MainActivity.java
+++ helloworld/src/main/java/com/example/helloworld/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView zs_text = (TextView) findViewById(R.id.zs_text);
+        zs_text.setText("你好，世界！");
+        zs_text.setTextColor(Color.RED);
+        zs_text.setTextSize(30);
     }
 }
