c462026e329de39e9928193b1b35b1213eba71a6
wuzengshun <823767678@qq.com>
2020-01-31 21:13:17 +0800

    增加 CaptureActivity 实现视图截图功能

---
+++ junior/src/main/java/com/example/junior/CaptureActivity.java
@@ -0,0 +1,71 @@
+package com.example.junior;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.junior.util.DateUtil;
+
+public class CaptureActivity extends AppCompatActivity  implements View.OnClickListener, View.OnLongClickListener {
+
+    private TextView tv_capture;
+    private ImageView iv_capture;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_capture);
+
+        tv_capture = (TextView) findViewById(R.id.tv_capture);
+        iv_capture = (ImageView) findViewById(R.id.iv_capture);
+        tv_capture.setDrawingCacheEnabled(true);
+
+        Button btn_chat = (Button) findViewById(R.id.btn_chat);
+        Button btn_capture = (Button) findViewById(R.id.btn_capture);
+
+        btn_chat.setOnClickListener(this);
+        btn_chat.setOnLongClickListener(this);
+        btn_capture.setOnClickListener(this);
+    }
+
+    private String[] mChatStr = { "你吃饭了吗？", "今天天气真好呀。",
+            "我中奖啦！", "我们去看电影吧。", "晚上干什么好呢？" };
+
+    public boolean onLongClick(View v) {
+        if (v.getId() == R.id.btn_chat) {
+            tv_capture.setText("");
+        }
+        return true;
+    }
+
+    public void onClick(View v) {
+        if (v.getId() == R.id.btn_chat) {
+            int random = (int)(Math.random()*10) % 5;
+            String newStr = String.format("%s\n%s %s",
+                    tv_capture.getText().toString(), DateUtil.getNowTime(), mChatStr[random]);
+            tv_capture.setText(newStr);
+        } else if (v.getId() == R.id.btn_capture) {
+            Bitmap bitmap = tv_capture.getDrawingCache();
+            iv_capture.setImageBitmap(bitmap);
+            // 注意这里在截图完毕后不能马上关闭绘图缓存，因为画面渲染需要时间，
+            // 如果立即关闭缓存，渲染画面就会找不到位图对象，会报错
+            // “java.lang.IllegalArgumentException: Cannot draw recycled bitmaps”。
+            mHandler.postDelayed(mResetCache, 200);
+        }
+    }
+
+    private Handler mHandler = new Handler();
+    private Runnable mResetCache = new Runnable() {
+        @Override
+        public void run() {
+            tv_capture.setDrawingCacheEnabled(false);
+            tv_capture.setDrawingCacheEnabled(true);
+        }
+    };
+}
