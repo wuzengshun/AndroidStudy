c2d9603a6909d2ddc3bcdeaa5fde12e18e15d426
wuzengshun <823767678@qq.com>
2020-02-01 13:40:42 +0800

    增加 StateActivity 实现按键根据状态显示不同的drawable图像

---
+++ junior/src/main/java/com/example/junior/StateActivity.java
@@ -0,0 +1,14 @@
+package com.example.junior;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_state);
+    }
+}
