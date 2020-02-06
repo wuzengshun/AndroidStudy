ce7949773e8e34d1d26c69998adf7e49fd62df6f
wuzengshun <823767678@qq.com>
2020-02-06 15:59:33 +0800

    增加 RadioVerticalActivity 模块实现竖向单选按钮

---
+++ middle/src/main/java/com/example/middle/RadioVerticalActivity.java
@@ -0,0 +1,14 @@
+package com.example.middle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RadioVerticalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_radio_vertical);
+    }
+}
