2c55b5203c8f1ef8fddb73ca90b30a583b6da4dc
wuzengshun <823767678@qq.com>
2020-02-07 16:42:37 +0800

    增加 SpinnerDropdownActivity 模块实现下拉式下拉框

---
+++ middle/src/main/java/com/example/middle/SpinnerDropdownActivity.java
@@ -0,0 +1,40 @@
+package com.example.middle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class SpinnerDropdownActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spinner_dropdown);
+
+        ArrayAdapter<String> starAdapter = new ArrayAdapter<String>(this,
+                R.layout.item_select, starArray);
+        starAdapter.setDropDownViewResource(R.layout.item_dropdown);
+
+        Spinner sp = (Spinner) findViewById(R.id.sp_dropdown);
+        sp.setPrompt("请选择行星");
+        sp.setAdapter(starAdapter);
+        sp.setSelection(0);
+        sp.setOnItemSelectedListener(new MySelectedListener());
+    }
+
+    private String[] starArray = {"水星", "金星", "地球", "火星", "木星", "土星"};
+    class MySelectedListener implements OnItemSelectedListener {
+        public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+            Toast.makeText(SpinnerDropdownActivity.this, "您选择的是"+starArray[arg2], Toast.LENGTH_LONG).show();
+        }
+
+        public void onNothingSelected(AdapterView<?> arg0) {
+        }
+    }
+}
