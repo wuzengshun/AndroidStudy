6bda5eaeb2ff22ce9c3c75b10be09599d7c77151
wuzengshun <823767678@qq.com>
2020-02-23 19:44:26 +0800

    增加 ShareWriteActivity 模块实现保存共享参数SharedPreferences

---
+++ storage/src/main/java/com/example/storage/util/DateUtil.java
@@ -0,0 +1,31 @@
+package com.example.storage.util;
+
+import android.annotation.SuppressLint;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateUtil {
+    @SuppressLint("SimpleDateFormat")
+    public static String getNowDateTime(String formatStr) {
+        String format = formatStr;
+        if (format==null || format.length()<=0) {
+            format = "yyyyMMddHHmmss";
+        }
+        SimpleDateFormat s_format = new SimpleDateFormat(format);
+        return s_format.format(new Date());
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    public static String getNowTime() {
+        SimpleDateFormat s_format = new SimpleDateFormat("HH:mm:ss");
+        return s_format.format(new Date());
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    public static String getNowTimeDetail() {
+        SimpleDateFormat s_format = new SimpleDateFormat("HH:mm:ss.SSS");
+        return s_format.format(new Date());
+    }
+}
